a0d53d8a6200f6687d8cf2fc1f5e66d0a579ce14
regbo <reggie.pierce@gmail.com>
2026-01-10 14:04:17 -0500

    Update pyproject.py

--- src/lfp_build/pyproject.py
+++ src/lfp_build/pyproject.py
@@ -307,7 +307,9 @@ def _taplo_commands() -> list[str] | None:
     program = "taplo"
     for commands in [[program], ["uv", "tool", "run", "--", program]]:
         try:
-            if util.process_run(commands[0], *commands[1:], "--version", stderr_log_level=None)
+            if util.process_run(
+                commands[0], *commands[1:], "--version", stderr_log_level=None
+            ):
                 return commands
         except Exception:
             continue
